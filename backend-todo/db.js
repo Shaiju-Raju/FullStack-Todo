0360941ab1499e3fa09702c4e681f4909bda0400
Shaiju Raju <shaijur2014@gmail.com>
2026-01-18 13:09:41 +0530

    Fix DB connection using DATABASE_URL for Render

--- backend-todo/db.js
+++ backend-todo/db.js
@@ -1,12 +1,27 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); // 🔥 THIS LINE WAS MISSING
+
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todos",
-  password: "Shark2014#",
-  port: 5432,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // ✅ Production (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  // ✅ Local Development
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 export default pool;
--- backend-todo/index.js
+++ backend-todo/index.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 
 import authRoutes from "./routes/auth.js";
 import todoRoutes from "./routes/todos.js";
 
-dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT;
